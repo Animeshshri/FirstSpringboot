468c8d04e644affb8d1efacc3b90e5086b412b24
Animeshshri <38254022+Animeshshri@users.noreply.github.com>
2021-04-19 17:51:05 +0530

    first commit

--- src/test/java/spring/demo/first/FirstApplicationTests.java
+++ src/test/java/spring/demo/first/FirstApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FirstApplicationTests {
-
+//comment added
 	@Test
 	public void contextLoads() {
 	}
